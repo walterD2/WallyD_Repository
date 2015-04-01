c3b6dca12d58cfa460a5807f100328ee6b14c27e
Walter DeArmond <walter.dearmond@gmail.com>
2015-04-01 17:50:38 -0400

    Initial Check in - Git test

---
+++ GitRepositoryTest/Java Project 1/Java Project 1.java
@@ -0,0 +1,8 @@
+public class Java Project 1 {
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+	System.out.println ("Hello World!");
+	System.out.println("The First Commit");
+    }
+}
